191b28a2e5609fb22ba96512eb6c48dc82db4af3
Ninh Nguyen (阮安宁) <ninhnguyenweb@gmail.com>
2022-08-26 20:52:21 -0700

    Add files via upload

---
+++ JSessential/03_07/script.js
@@ -0,0 +1,21 @@
+/**
+ * Create a Car object.
+ * By Ninh Nguyen.
+ */
+
+const car = {
+    Name: "My Baby",
+    Brand: "Toyota",
+    Model: "Camry SE",
+    Door: 4,
+    Type: "Sedan",
+    Color: "White",
+
+};
+
+console.log("Ninh's Car:", car);
+console.log("Ninh's Car:", car.Name);
+
+var query = "Door";
+
+console.log("Ninh's Car Door:", car[query]);
